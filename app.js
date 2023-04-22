384a49ce06e49988c2915c99f18fa8ff38204f7e
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-22 23:56:07 +0900

    routes/auth.js - 테스트 결과 주석 추가

--- app.js
+++ app.js
@@ -25,3 +25,27 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
 });
+
+
+/*
+
+localhost:3000/api/auth
+
+{
+  "email": "raxniper@naver.com",
+  "password": "qwer1234"
+}
+
+POST 요청 해보면
+
+Response
+{
+  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDQzZGNjYmFjNjdjMWU4NTE3ZjQxZjYiLCJpYXQiOjE2ODIxNzUxOTd9.0uC-vuquE0MHRgKUyu6yXUdE73FXPr5l16MB5AJgDeI"
+}
+
+Cookies
+Name : authorization
+Value : Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDQzZGNjYmFjNjdjMWU4NTE3ZjQxZjYiLCJpYXQiOjE2ODIxNzUxOTd9.0uC-vuquE0MHRgKUyu6yXUdE73FXPr5l16MB5AJgDeI
+-> Value에 Bearer 형태의 값이 들어와있음
+
+*/
