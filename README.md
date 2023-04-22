f7603730f1f2ac8dfba52fbe0f21621f98af611d
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-22 17:00:24 +0900

    미들웨어 연습중

---
+++ README.md
@@ -0,0 +1,11 @@
+# PRACTICE. SPA MALL
+
+### RESTful API
+
+### Middle Ware
+  - Router / Middle Ware 차이
+    * Router는 Middle Ware 기반으로 구현된 객체이므로 동일한 방식으로 작동함
+  - Express.js Middle Ware 실행되는 경우
+    * app.use(M/W) : 모든 요청에 미들웨어가 실행
+    * app.use('/api', M/W) : api로 시작하는 요청에서 미들웨어가 실행
+    * app.post('/api', M/W) : api로 시작하는 POST(외 HTTP Method) 요청에서 미들웨어가 실행
--- app.js
+++ app.js
@@ -1,23 +1,42 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
-const port = 3000;
+const connect = require('./schemas');
+const goodsRouter = require('./routes/goods');
+const cartsRouter = require('./routes/carts');
 
-app.use(express.json())
+connect();
+
+app.use(express.json());  // post, put 전달된 body 데이터를 req.body로 사용할 수 있도록 만든 body-parser
+
+app.use((req, res, next) => {   // 밑에 있는 Router 미들웨어 위에 있어야 함
+  console.log('Request URL: ', req.originalUrl, ' - ', new Date());
+  next();
+});
+
+app.use((req, res, next) => {   // 밑에 있는 Router 미들웨어 위에 있어야 함
+  console.log('첫번째 미들웨어');
+  next();
+});
+
+app.use((req, res, next) => {   // 밑에 있는 Router 미들웨어 위에 있어야 함
+  console.log('두번째 미들웨어');
+  next();
+});
+
+app.use((req, res, next) => {   // 밑에 있는 Router 미들웨어 위에 있어야 함
+  console.log('세번째 미들웨어');
+  next();
+});
+
+// app.use('/api', [goodsRouter, cartsRouter]);  // API가 사용되기 위한 라우터 등록
+app.use('/api', [goodsRouter]);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const goodsRouter = require('./routes/goods');
-app.use('/api', [goodsRouter]);
-
-// const cartsRouter = require('./routes/carts');
-// app.use('/api', [goodsRouter, cartsRouter]);
-
-const connect = require('./schemas');
-connect();
-
-app.listen(port, () => {
-  console.log(port, '포트로 서버가 열렸어요!');
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸어요!');
 });
