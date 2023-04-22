64bda9f84c0570662973581edea668a00a7f2307
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-22 21:55:35 +0900

    add.js - usersRouter 등록

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 
 const goodsRouter = require("./routes/goods.js");
 const cartsRouter = require("./routes/carts.js");
+const usersRouter = require("./routes/users.js");
 
 const connect = require("./schemas");
 connect();
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("assets"));
 
-app.use("/api", [goodsRouter, cartsRouter]);
+app.use("/api", [goodsRouter, cartsRouter, usersRouter]);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
