8a3fd9eeb2f48e3fb771cf6cac55c6c3fb3975bd
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-24 17:16:32 +0900

    routes/goods.js - 상품조회 API 기능 추가

--- routes/goods.js
+++ routes/goods.js
@@ -4,6 +4,44 @@ const Goods = require("../schemas/goods.js");
 const Cart = require("../schemas/cart.js");
 
 
+// 상품 등록 API
+router.post("/goods", async (req, res) => {
+  const { goodsId, name, thumbnailUrl, category, price } = req.body;
+
+  const goods = await Goods.find({ goodsId });
+
+  if (goods.length) {
+    return res.status(400).json({
+      success: false,
+      errorMessage: "이미 존재하는 GoodsId입니다."
+    });
+  }
+
+  const createdGoods = await Goods.create({ goodsId, name, thumbnailUrl, category, price });
+
+  res.json({ goods: createdGoods });
+})
+
+
+// 상품 조회 API
+router.get("/goods", async (req, res) => {
+  const { category } = req.query;
+  const goods = await Goods.find({category})
+    .sort("-date")  // 생성시간을 통해 내림차순 정렬
+    .exec();        // 쿼리 마무리
+  const results = goods.map((item) => {
+    return {
+      goodsId: item.goodsId,
+      name: item.name,
+      price: item.price,
+      thumbnailUrl: item.thumbnailUrl,
+      category: item.category
+    }
+  });
+
+  return res.status(200).json({goods: results})
+});
+
 
 // 상품 장바구니 등록 API
 router.post("/goods/:goodsId/cart", async (req, res) => {
